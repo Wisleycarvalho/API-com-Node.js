649ddacd4665c6b73ce92bba7681962f86076682
Wisley <wisley.dev@gmail.com>
2024-06-10 23:11:00 -0300

    Primeiro commit do meu projeto

---
+++ server.js
@@ -0,0 +1,54 @@
+import express from 'express';
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient()
+
+
+const app = express();
+app.use(express.json())
+
+app.post('/users', async (req, res) => {
+    
+    await prisma.user.create({
+        data: {
+            name: req.body.name,
+            email: req.body.email,
+            age: req.body.age
+        }
+            
+    })
+
+    res.send(201).json(req.body)
+});
+
+app.put('/users/:id', async (req, res) => {
+    
+    console.log(req)
+    await prisma.user.update({
+        where: {
+            id: req.params.id
+        },
+        data: {
+            name: req.body.name,
+            email: req.body.email,
+            age: req.body.age
+        }
+            
+    })
+
+    res.send(201).json(req.body)
+});
+
+app.get('/users', async (req, res)=>{
+    
+    const users = await prisma.user.findMany()
+
+    res.status(200).json(users)
+
+});
+
+app.delete('/users/:id', async (req, res) => {
+    await prisma.user.delete
+})
+
+app.listen(3000)
